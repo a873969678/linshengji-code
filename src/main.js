6955d49e7032cfa02b5815fc51d2971296edf09c
linshengji <873969678@qq.com>
2019-11-27 11:10:37 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
@@ -14,7 +10,6 @@ import store from './store'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 Vue.prototype.GLOBAL = {
