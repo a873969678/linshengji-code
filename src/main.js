ab6e76eb17f1ffb1fb1babd9342b1266d1b94626
linshengji <873969678@qq.com>
2019-11-27 14:02:52 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
 import router from './router'
-import store from './store'
-
-import '@/icons' // icon
-import '@/permission' // permission control
 
 Vue.config.productionTip = false
 Vue.prototype.GLOBAL = {
@@ -18,6 +12,5 @@ Vue.prototype.GLOBAL = {
 export default new Vue({
   el: '#app',
   router,
-  store,
   render: h => h(App)
 })
